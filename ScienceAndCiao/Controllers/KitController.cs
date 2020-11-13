c4038588d9a7e5195567df784513424085352885
lyankey <56731939+lyankey@users.noreply.github.com>
2020-11-13 11:43:12 -0500

    Add project files.

---
+++ ScienceAndCiao/Controllers/KitController.cs
@@ -0,0 +1,19 @@
+﻿using ScienceAndCiao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScienceAndCiao.Controllers
+{
+    public class KitController : Controller
+    {
+        // GET: Kit/Random
+        public ActionResult Random()
+        {
+            var kit = new Kit() { Name = "First Kit" };
+            return View(kit);
+        }
+    }
+}
---
+++ ScienceAndCiao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScienceAndCiao.Startup))]
+namespace ScienceAndCiao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
